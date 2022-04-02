b852562a49012db1f3d978594dca44b131689554
AAAsniperAAA <67518222+AAAsniperAAA@users.noreply.github.com>
2022-04-02 15:44:28 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,55 +1,43 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, motor_speed + 10)
-        motor_speed += 10
+        if (Reverse_speed == 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, motor_speed + 10)
+            motor_speed += 10
+        }
+        if (motor_speed == 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, Reverse_speed - 10)
+            Reverse_speed += -10
+        }
     }
     if (receivedNumber == 2) {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, motor_speed - 10)
-        motor_speed += -10
+        if (Reverse_speed == 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, motor_speed - 10)
+            motor_speed += -10
+        }
+        if (motor_speed == 0) {
+            Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, Reverse_speed + 10)
+            Reverse_speed += 10
+        }
     }
     if (receivedNumber == 3) {
         Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 0)
     }
     if (receivedNumber == 4) {
-        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 90)
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 180)
     }
-    if (receivedNumber == 5) {
-        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Forward, 10)
-    }
-    if (receivedNumber == 6) {
-        Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor4)
-    }
+    basic.showIcon(IconNames.Yes)
+    basic.clearScreen()
 })
+let Reverse_speed = 0
 let motor_speed = 0
 radio.setGroup(15)
 motor_speed = 0
+Reverse_speed = 0
 basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Up)) {
-        radio.sendNumber(1)
+    if (motor_speed > 100) {
+        motor_speed = 100
     }
-})
-basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Fire1)) {
-        radio.sendNumber(3)
-    }
-})
-basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
-        radio.sendNumber(5)
-    }
-})
-basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Down)) {
-        radio.sendNumber(2)
-    }
-})
-basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Fire2)) {
-        radio.sendNumber(4)
-    }
-})
-basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left)) {
-        radio.sendNumber(6)
+    if (Reverse_speed > 100) {
+        Reverse_speed = 100
     }
 })
